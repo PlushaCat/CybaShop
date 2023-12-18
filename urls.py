38b0d588ac7901facaefeff85bf7fba0c73a2b75
BuildTools <unconfigured@null.spigotmc.org>
2023-12-19 00:38:20 +0300

    update1

--- urls.py
+++ urls.py
@@ -18,7 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-import settings
+from Shop import settings
 
 urlpatterns = [
     path('', include("Shop.urls")),
